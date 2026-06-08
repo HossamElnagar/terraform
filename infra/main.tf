157c3848a55f4309a80da88702d5254f2c761679
hossam <hossam7009@gmail.com>
2026-06-08 10:04:26 +0000

    jjg

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,8 @@
-# VPC
 resource "aws_vpc" "main" {
-  cidr_block           = 10.0.0.0/16
+  cidr_block       = var.vpc_cidr
   instance_tenancy = "default" 
 
   tags = {
-    Name = "vpc_name"
+    Name = var.vpc_name
   }
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,17 +1,14 @@
 variable "aws_region" {
   type        = string
-  description = "AWS region" 
-  default     = "eu-west-1" # تم نقل القيمة إلى هنا
+  description = "AWS region"
 }
 
 variable "vpc_cidr" {
   type        = string
   description = "VPC cidr"
-  default     = "10.0.0.0/16" # تم نقل القيمة إلى هنا وإضافة علامات التنصيص
 }
 
 variable "vpc_name" {
   type        = string
   description = "VPC Name"
-  default     = "my-main-vpc" # أضفنا قيمة افتراضية لاسم الـ VPC
 }
