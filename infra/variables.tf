c53324be2ed6066c318b6a29100e278198cd445e
hossam <hossam7009@gmail.com>
2026-06-08 09:35:15 +0000

    gg

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,40 +1,17 @@
 variable "aws_region" {
   type        = string
-  description = "المنطقة اللي هنبني فيها اللاب في AWS"
-}
-
-variable "project_name" {
-  type        = string
-  description = "اسم المشروع اللي هيتكتب في التاجز لتنظيم الموارد"
+  description = "AWS region" 
+  default     = "eu-west-1" # تم نقل القيمة إلى هنا
 }
 
 variable "vpc_cidr" {
   type        = string
-  description = "نطاق الآي بي الخاص بالشبكة الكبيرة VPC"
-}
-
-variable "public_subnets" {
-  type        = list(string)
-  description = "قائمة بنطاقات الآي بي للـ Subnets العامة"
-}
-
-variable "availability_zones" {
-  type        = list(string)
-  description = "قائمة بمناطق التوفر داخل الريجون"
-}
-variable "instance_type" {
-  type        = string
-  description = "حجم السيرفر الصغير اللي هيشتغل جوة الأوتو سكيلنج"
-  default     = "t2.micro"
-}
-
-variable "server_port" {
-  type        = number
-  description = "البورت اللي السيرفرات والـ Load Balancer هيسمعوا عليه (HTTP)"
-  default     = 80
+  description = "VPC cidr"
+  default     = "10.0.0.0/16" # تم نقل القيمة إلى هنا وإضافة علامات التنصيص
 }
 
 variable "vpc_name" {
-  description = "VPC 1"
   type        = string
+  description = "VPC Name"
+  default     = "my-main-vpc" # أضفنا قيمة افتراضية لاسم الـ VPC
 }
